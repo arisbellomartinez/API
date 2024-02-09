c637775cf4039034215ae1a959a60b63c09b8772
Aris Bello M <arisbellomartinez@gmail.com>
2024-02-09 12:08:21 -0500

    Token echo

--- src/middleware/authToken.js
+++ src/middleware/authToken.js
@@ -4,17 +4,21 @@ import { db } from "../config/db/db.js";
 
 
 export const verifyToken = async (req, res,next) => {
-    const token = req.headers["x-access-token"]
-    const {usuario} = req.body
+    try {
+        const token = req.headers["x-access-token"]
     if (!token) {
         return res.status(403).json({message: " no Access token"})
     }
-    const decoded=jwt.verify(token,SECRET_KEY)
-    const [rows] = await db.query("SELECT * FROM auth WHERE usuario = ?",[usuario])
+    const decoded= jwt.verify(token,SECRET_KEY)
+    
+    const [rows] = await db.query("SELECT * FROM auth WHERE id = ?",[decoded.id])
+
+
     if (rows.length==0) {
         return res.status(404).json({message: "user does not exist"})
     }
-    
-    console.log(rows);
     next();
+    } catch (error) {
+        return res.status(401).json({message:"Unauthorized"})
+    }
 }
