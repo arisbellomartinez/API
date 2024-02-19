b173ee994aabc3138bf3420cc2a3485a3c5c43e9
Aris Bello M <arisbellomartinez@gmail.com>
2024-02-19 05:03:15 -0500

    test put

--- src/test/app.test.js
+++ src/test/app.test.js
@@ -1,15 +1,22 @@
 import app from "../app.js"
 import request from "supertest"
 
+const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MjYsImlhdCI6MTcwODMzNjc3MywiZXhwIjoxNzA4NDIzMTczfQ.FtI9ZQEZ7tiAxcL3KgkUv9UAWmycaz5xr1-sLTntJXA"
+
 describe("GET /tasks",() => {
     test("Should respond with 200 status code",async() => {
-        const response= await request(app).get("/tasks/").set("x-access-token","eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MjYsImlhdCI6MTcwODE4NDc1NCwiZXhwIjoxNzA4MjcxMTU0fQ.TN1RV1fDg2bDH8bvVIccbFkn2QiKeTckt9pWbZy197g").send()
+        const response= await request(app).get("/tasks/").set("x-access-token",token).send()
         expect(response.statusCode).toBe(200)
     })
 
     test("Should respond with an array of tasks",async() => {
-        const response= await request(app).get("/tasks/").set("x-access-token","eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MjYsImlhdCI6MTcwODE4NDc1NCwiZXhwIjoxNzA4MjcxMTU0fQ.TN1RV1fDg2bDH8bvVIccbFkn2QiKeTckt9pWbZy197g").send()
+        const response= await request(app).get("/tasks/").set("x-access-token",token).send()
         expect(response.body).toBeInstanceOf(Array)
+    })
+
+    test("Should respond with 400 status code",async() => {
+        const response= await request(app).get("/tasks/").set("x-access-token","token").send()
+        expect(response.statusCode).toBe(401)
     })
 
     
@@ -17,7 +24,7 @@ describe("GET /tasks",() => {
 
 describe("POST /tasks",()=>{
     test("Should respond with 200 status code",async()=>{
-        const response= await request(app).post("/tasks/").set("x-access-token","eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MjYsImlhdCI6MTcwODE4NDc1NCwiZXhwIjoxNzA4MjcxMTU0fQ.TN1RV1fDg2bDH8bvVIccbFkn2QiKeTckt9pWbZy197g").send({
+        const response= await request(app).post("/tasks/").set("x-access-token",token).send({
             "title":"test",
             "description":"test",
             "status": "test"
@@ -26,10 +33,21 @@ describe("POST /tasks",()=>{
     })
 
     test("Should respond with 400 the created task",async()=>{
-        const response= await request(app).post("/tasks/").set("x-access-token","eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MjYsImlhdCI6MTcwODE4NDc1NCwiZXhwIjoxNzA4MjcxMTU0fQ.TN1RV1fDg2bDH8bvVIccbFkn2QiKeTckt9pWbZy197g").send({
+        const response= await request(app).post("/tasks/").set("x-access-token",token).send({
             "title":"test",
             "description":"test",
         })
         expect(response.statusCode).toBe(400)
+    })
+})
+
+describe("PUT /tasks/:id",()=>{
+    test("Should respond with 200",async()=>{
+        const response= await request(app).put("/tasks/10").set("x-access-token",token).send({
+            "title":"test put",
+            "description":"test put",
+            "status": "test put"
+        })
+        expect(response.statusCode).toBe(200)
     })
 })
