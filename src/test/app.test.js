a7530a2945396bfe5782c0436fa843bb5ac01af5
Aris Bello M <arisbellomartinez@gmail.com>
2024-02-19 05:10:17 -0500

    buenas practicas

--- src/test/app.test.js
+++ src/test/app.test.js
@@ -1,60 +1,63 @@
-import app from "../app.js"
-import request from "supertest"
+import app from "../app.js";
+import request from "supertest";
 
-const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MjYsImlhdCI6MTcwODMzNjc3MywiZXhwIjoxNzA4NDIzMTczfQ.FtI9ZQEZ7tiAxcL3KgkUv9UAWmycaz5xr1-sLTntJXA"
+// Define a valid JWT token for authorization
+const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MjYsImlhdCI6MTcwODMzNjc3MywiZXhwIjoxNzA4NDIzMTczfQ.FtI9ZQEZ7tiAxcL3KgkUv9UAWmycaz5xr1-sLTntJXA";
 
-describe("GET /tasks",() => {
-    test("Should respond with 200 status code",async() => {
-        const response= await request(app).get("/tasks/").set("x-access-token",token).send()
-        expect(response.statusCode).toBe(200)
-    })
+// Test suite for GET /tasks route
+describe("GET /tasks", () => {
+    test("Should respond with 200 status code", async () => {
+        const response = await request(app).get("/tasks/").set("x-access-token", token).send();
+        expect(response.statusCode).toBe(200);
+    });
 
-    test("Should respond with an array of tasks",async() => {
-        const response= await request(app).get("/tasks/").set("x-access-token",token).send()
-        expect(response.body).toBeInstanceOf(Array)
-    })
+    test("Should respond with an array of tasks", async () => {
+        const response = await request(app).get("/tasks/").set("x-access-token", token).send();
+        expect(response.body).toBeInstanceOf(Array);
+    });
 
-    test("Should respond with 400 status code",async() => {
-        const response= await request(app).get("/tasks/").set("x-access-token","token").send()
-        expect(response.statusCode).toBe(401)
-    })
+    test("Should respond with 401 status code for invalid token", async () => {
+        const response = await request(app).get("/tasks/").set("x-access-token", "invalid-token").send();
+        expect(response.statusCode).toBe(401);
+    });
+});
 
-    
-})
-
-describe("POST /tasks",()=>{
-    test("Should respond with 200 status code",async()=>{
-        const response= await request(app).post("/tasks/").set("x-access-token",token).send({
-            "title":"test",
-            "description":"test",
+// Test suite for POST /tasks route
+describe("POST /tasks", () => {
+    test("Should respond with 200 status code", async () => {
+        const response = await request(app).post("/tasks/").set("x-access-token", token).send({
+            "title": "test",
+            "description": "test",
             "status": "test"
-        })
-        expect(response.statusCode).toBe(200)
-    })
+        });
+        expect(response.statusCode).toBe(200);
+    });
 
-    test("Should respond with 400 the created task",async()=>{
-        const response= await request(app).post("/tasks/").set("x-access-token",token).send({
-            "title":"test",
-            "description":"test",
-        })
-        expect(response.statusCode).toBe(400)
-    })
-})
+    test("Should respond with 400 for incomplete task data", async () => {
+        const response = await request(app).post("/tasks/").set("x-access-token", token).send({
+            "title": "test",
+            "description": "test"
+        });
+        expect(response.statusCode).toBe(400);
+    });
+});
 
-describe("PUT /tasks/:id",()=>{
-    test("Should respond with 200",async()=>{
-        const response= await request(app).put("/tasks/15").set("x-access-token",token).send({
-            "title":"test put",
-            "description":"test put",
+// Test suite for PUT /tasks/:id route
+describe("PUT /tasks/:id", () => {
+    test("Should respond with 200 status code", async () => {
+        const response = await request(app).put("/tasks/15").set("x-access-token", token).send({
+            "title": "test put",
+            "description": "test put",
             "status": "test put"
-        })
-        expect(response.statusCode).toBe(200)
-    })
-})
+        });
+        expect(response.statusCode).toBe(200);
+    });
+});
 
-describe("DELETE /tasks/:id",()=>{
-    test("Should respond with 200",async()=>{
-        const response= await request(app).delete("/tasks/12").set("x-access-token",token).send()
-        expect(response.statusCode).toBe(200)
-    })
-})
+// Test suite for DELETE /tasks/:id route
+describe("DELETE /tasks/:id", () => {
+    test("Should respond with 200 status code", async () => {
+        const response = await request(app).delete("/tasks/12").set("x-access-token", token).send();
+        expect(response.statusCode).toBe(200);
+    });
+});
